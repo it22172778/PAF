fdcedaac42dd69acf3a5a6c9d08f95dc9972bf06
Almaas <it22172778@my.sliit.lk>
2025-04-25 19:39:26 +0530

    model and dto forTravelChallenge

---
+++ demo/src/main/java/com/example/demo/dto/TravelChallengeSharingDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class TravelChallengeSharingDto {
+    private String travellerName;
+    private String travellerPhoneNo;
+    private String challengeTitle;
+    private String challengeDescription;
+    private String travellerAdvice;
+}
