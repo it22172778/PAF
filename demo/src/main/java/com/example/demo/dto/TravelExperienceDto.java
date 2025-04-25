eaa49c6f38757534c9d3e1ebdf1d0ec28355ca3f
Zamran Ahamed <zamranahamed7@gmail.com>
2025-04-25 21:10:36 +0530

    TravelExperienceDto Adjustment and TravelExperienceRepository created

--- demo/src/main/java/com/example/demo/dto/TravelExperienceDto.java
+++ demo/src/main/java/com/example/demo/dto/TravelExperienceDto.java
@@ -3,9 +3,15 @@ package com.example.demo.dto;
 import java.time.LocalDate;
 import java.util.List;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelExperienceDto {
     private String title;
     private String location;
