e2465de0bdfb37ecc36689fd323fdd4ce6418eae
Almaas <it22172778@my.sliit.lk>
2025-04-25 19:52:28 +0530

    dto updateed

--- demo/src/main/java/com/example/demo/dto/TravelChallengeSharingDto.java
+++ demo/src/main/java/com/example/demo/dto/TravelChallengeSharingDto.java
@@ -1,8 +1,14 @@
 package com.example.demo.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelChallengeSharingDto {
     private String travellerName;
     private String travellerPhoneNo;
